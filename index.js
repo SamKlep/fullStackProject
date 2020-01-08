8f87c0bbe4529579a08c862fe014529527daebfa
kev <45981465+abbo-1@users.noreply.github.com>
2020-01-08 17:49:17 -0600

    began integrating passport.js and ejs

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const models = require('./models');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+app.set('view-engine', 'ejs')
+
 app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'ejs')
 
