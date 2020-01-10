03227ad5f59534259bdaca5b6f0db9674d357e61
kev <45981465+abbo-1@users.noreply.github.com>
2020-01-09 21:40:54 -0600

    no new functionality but close to having local authentication working using passport.

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const models = require('./models');
+const passport = require('passport');
+const session = require('express-session');
+
+const initalizePassport = require('./passport-config');
+initalizePassport(passport);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -9,6 +14,14 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.set('view-engine', 'ejs')
 
 app.use(express.static(__dirname + '/public'));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(session({
+    secret: 'redwine',
+    resave: false;
+    saveUninitialized: false;
+}))
+
 app.set('view engine', 'ejs')
 
 ////////////////////////////////////
