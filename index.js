5fa8d35d157cda60fdc8782cfaafeac837aa85fa
Samuel Klepper <jmmdgm@samuel-fx.local>
2020-01-10 13:51:24 -0600

    created models for users, wines, and beers tables

--- index.js
+++ index.js
@@ -2,11 +2,28 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const models = require('./models');
+const passport = require('passport');
+const session = require('express-session');
+
+/*
+const initalizePassport = require('./passport-config');
+initalizePassport(passport);
+*/
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+app.set('view-engine', 'ejs')
+
 app.use(express.static(__dirname + '/public'));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(session({
+    secret: 'redwine',
+    resave: false,
+    saveUninitialized: false,
+}))
+
 app.set('view engine', 'ejs')
 
 ////////////////////////////////////
@@ -38,6 +55,37 @@ app.delete("/drinks", function (req, response) {
     response.send("item deleted");
 });
 
+///////////////////////////
+///Querying the database///
+///////////////////////////
+/* Find all users
+User.findAll().then(users => {
+    console.log("All users:", JSON.stringify(users, null, 4));
+  });
+  
+  // Create a new user
+  User.create({ firstName: "Jane", lastName: "Doe" }).then(jane => {
+    console.log("Jane's auto-generated ID:", jane.id);
+  });
+  
+  // Delete everyone named "Jane"
+  User.destroy({
+    where: {
+      firstName: "Jane"
+    }
+  }).then(() => {
+    console.log("Done");
+  });
+  
+  // Change everyone without a last name to "Doe"
+  User.update({ lastName: "Doe" }, {
+    where: {
+      lastName: null
+    }
+  }).then(() => {
+    console.log("Done");
+  });
+  */
 
 app.listen(8080, function () {
     console.log('Example app listening on port 8080!');
