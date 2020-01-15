0fb50f7944172093d5b1fac15f196b67466b2380
Samuel Klepper <jmmdgm@samuel-fx.local>
2020-01-14 20:53:06 -0600

    changed html to ejs

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.use(session({
 
 app.set('view engine', 'ejs');
 app.set("views", __dirname + "/views");
-app.engine('html', require('ejs').renderFile);
+
 
 
 //////////Express Routes////////////////////////
