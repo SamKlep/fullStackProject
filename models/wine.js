77576cc5ea0a5ac85426afd7d0e755394ff502ca
Samuel Klepper <55067844+SamKlep@users.noreply.github.com>
2020-02-05 19:05:06 -0600

    added relations

--- models/wine.js
+++ models/wine.js
@@ -3,6 +3,8 @@ module.exports = (sequelize, DataTypes) => {
   const wine = sequelize.define('wine', {
     name: DataTypes.STRING,
     type: DataTypes.STRING,
+    vineyard: DataTypes.STRING,
+    vintage: DataTypes.STRING,
     date: DataTypes.INTEGER,
     description: DataTypes.STRING,
     rating: DataTypes.STRING
